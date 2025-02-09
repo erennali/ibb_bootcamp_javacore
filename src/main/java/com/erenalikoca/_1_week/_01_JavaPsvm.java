00af2d9bce4c7a7b870ce130faf3639f093dcac3
Eren Ali Koca <eren_ali_koca@hotmail.com>
2025-02-09 18:28:09 +0300

    psvm,sout tab chortcut öğrenildi

---
+++ src/main/java/com/erenalikoca/_1_week/_01_JavaPsvm.java
@@ -0,0 +1,14 @@
+package com.erenalikoca._1_week;
+
+//single comment
+/*
+multiple
+ */
+public class _01_JavaPsvm {
+        //psvm=> tab
+        public static void main(String[] args) {
+            //çıktı
+            //sout => tab
+            System.out.println("Eren Ali Koca");
+        }//end psvm
+}//end class

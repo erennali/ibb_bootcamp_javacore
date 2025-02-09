62e8a91d222814a0894f61f162299cf179aad809
Eren Ali Koca <eren_ali_koca@hotmail.com>
2025-02-09 18:58:52 +0300

    variables ve rules

---
+++ src/main/java/com/erenalikoca/_1_week/_03_JavaPrimitiveTypes.java
@@ -0,0 +1,36 @@
+package com.erenalikoca._1_week;
+
+public class _03_JavaPrimitiveTypes {
+    public static void main(String[] args) {
+
+        //Primitive types 8 tanedir , null değer alamaz
+
+        //tam sayılar 4
+        byte b1 = 127;
+        System.out.println("Byte: "+b1);
+
+        short s1 = 32767;
+        System.out.println("Short: "+s1);
+
+        int i1 = 2147483647;
+        System.out.println("Int: "+i1);
+
+        long l1 = 9223372036854775807L;
+        System.out.println("Long: "+l1);
+
+        //virgüllü (floating point)
+        float f2 = 3.14f;
+        System.out.println("Float: "+f2);
+
+        double d2 = 3.14;
+        System.out.println("Double: "+d2);
+
+        //boolean
+        boolean b3 = true;
+        System.out.println("Boolean: "+b3);
+
+        //char
+        char c4 = '\u4125';
+        System.out.println("Char: "+c4);
+    }
+}

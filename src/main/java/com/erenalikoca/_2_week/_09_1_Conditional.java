0294a5a1fc4c4d9e44051987f86a02f8fdd16867
Eren Ali Koca <eren_ali_koca@hotmail.com>
2025-02-13 14:15:25 +0300

    conditional

---
+++ src/main/java/com/erenalikoca/_2_week/_09_1_Conditional.java
@@ -0,0 +1,14 @@
+package com.erenalikoca._2_week;
+
+public class _09_1_Conditional {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else if (number < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
---
+++ src/main/java/com/erenalikoca/_2_week/_09_2_Conditional_Ternary.java
@@ -0,0 +1,9 @@
+package com.erenalikoca._2_week;
+
+public class _09_2_Conditional_Ternary {
+    public static void main(String[] args) {
+        int number = 5;
+        String result = ( number > 0 ) ? "Positive number" : "Negative number";
+        System.out.println(result);
+    }
+}
